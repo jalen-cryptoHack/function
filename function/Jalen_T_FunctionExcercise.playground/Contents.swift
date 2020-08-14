c10641f37fee8484031d46fb6679573c24da50f3
jalen-cryptoHack <67969173+jalen-cryptoHack@users.noreply.github.com>
2020-08-14 08:05:31 -0500

    Add files via upload

---
+++ function/Jalen_T_FunctionExcercise.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func calculator (num1: Int, num2: Int) -> Int {
+    var results = 0
+    results = num1 + num2
+    
+    return results
+}
+var total = calculator(num1: 36, num2: 12)
+print("36 + 12 = \(total)")
